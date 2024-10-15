a5e7c2710e8d1b57626666d33f3911e5ec2e7fd2
Hinal <hinalp2002@gmail.com>
2024-10-14 20:48:34 -0700

    test2

--- TripPlanner3/Controllers/PlanController.swift
+++ TripPlanner3/Controllers/PlanController.swift
@@ -15,7 +15,7 @@
 //    @Published var apiError: String? = nil  // Store any API errors to show in UI
 //
 //    // new API key
-//    private let openAI = OpenAI(apiToken: "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")
+//    private let openAI = OpenAI(apiToken: "temp")
 //
 //
 //    /// Clear previous search results before starting a new one
